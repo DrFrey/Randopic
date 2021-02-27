2d5a90f37f07f60ae16167fd7841a47205d66a9d
DrFrey <avorovatov@gmail.com>
2021-02-27 15:26:24 +0300

    added placeholders and header

--- app/src/main/java/com/example/randopic/BindingAdapters.kt
+++ app/src/main/java/com/example/randopic/BindingAdapters.kt
@@ -1,23 +1,45 @@
 package com.example.randopic
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import androidx.paging.PagingData
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.randopic.data.Picture
-import com.example.randopic.overview.PictureDataAdapter
-import kotlinx.coroutines.Job
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomTarget
 
-/*@BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView, data: PagingData<Picture>?) {
-    val adapter = recyclerView.adapter as PictureDataAdapter
-    adapter.submitData(data)
-}*/
 
 @BindingAdapter("imageUrl")
 fun loadImage(imageView: ImageView, url: String?) {
     if (!url.isNullOrEmpty()) {
-        Glide.with(imageView.context).load(url).into(imageView)
+        Glide.with(imageView.context)
+            .load(url)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )
+            .into(imageView)
+    }
+}
+
+@BindingAdapter("userpicUrl")
+fun loadImageIntoTextview(textView: TextView, url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(textView.context)
+            .load(url)
+            .circleCrop()
+            .into(object : CustomTarget<Drawable>(100, 100) {
+                override fun onLoadCleared(drawable: Drawable?) {
+                    textView.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+                }
+
+                override fun onResourceReady(
+                    res: Drawable,
+                    transition: com.bumptech.glide.request.transition.Transition<in Drawable>?
+                ) {
+                    textView.setCompoundDrawablesWithIntrinsicBounds(res, null, null, null)
+                }
+            })
     }
 }
